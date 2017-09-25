34eae6b081018f38f48247c0bb054da8f39c3103
Amanda Greenberry <green6erry@gmail.com>
2017-09-25 17:02:47 -0400

    Auto-generated commit

--- src/app/burger-icon/burger-icon.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-burger-icon',
-  template: ` <div ng-repeat="toggle in toggles"><div class="burger" ng-click="toggle = !toggle" ng-class="{'cross' : toggle}"><span></span></div></div>`,
-  styleUrls: ['./burger-icon.component.css']
-})
-
-export class BurgerIconComponent {}
