db93193c7c1e02e8ff72a9e3c37415feb613bc4d
Amanda Greenberry <green6erry@gmail.com>
2017-09-25 11:27:11 -0400

    Set width to KNote
    Testing out styling.

--- src/app/knowledge-note/knowledge-note.ts
+++ src/app/knowledge-note/knowledge-note.ts
@@ -1,7 +1,8 @@
 import { Input, Component } from '@angular/core';
 @Component ({
     selector: 'app-knowledge-note',
-    templateUrl: './knowledge-note.html'
+    templateUrl: './knowledge-note.html',
+    styles: ['img {width: 100%;}']
 })
 
 export class KnowledgeNote {
