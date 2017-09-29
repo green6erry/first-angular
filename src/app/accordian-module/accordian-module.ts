e3abd1f4a821a867313b5d829b88c098f0103b4e
Amanda Greenberry <green6erry@gmail.com>
2017-09-29 15:48:58 -0400

    recommit
    Whaaatttt

---
+++ src/app/accordian-module/accordian-module.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordian-module',
+  templateUrl: './accordian-module.html'
+})
+
+export class AccordianModule {}
---
+++ src/app/burger-icon/burger-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-burger-icon',
+  template: ` <div class="burger">
+    <span></span>
+    </div>`,
+  styleUrls: ['./burger-icon.component.css']
+})
+
+export class BurgerIconComponent {}
