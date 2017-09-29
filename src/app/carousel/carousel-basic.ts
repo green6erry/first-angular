2c79a13cf47d1aa98e11351d278c8652e980db81
Amanda Greenberry <green6erry@gmail.com>
2017-09-29 16:21:49 -0400

    gh-pages

--- src/app/carousel/carousel-basic.ts
+++ src/app/carousel/carousel-basic.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-carousel-basic',
-  templateUrl: './carousel-basic.html'
+  templateUrl: './carousel-basic.html',
+  styleUrls: ['./carousel-basic.css'] 
 })
 export class NgbdCarouselBasic {
 }
