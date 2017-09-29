3ae00171ac0ecac66f03e1036905a4b2268b96ee
Amanda Greenberry <green6erry@gmail.com>
2017-09-29 14:37:38 -0400

    Auto-generated commit

--- src/app/burger2/burger2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-burger2',
-  templateUrl: './burger2.html',
-  styleUrls: ['./burger2.css']
-})
-
-export class Burger2Component {}
